c92a636e379e910a657c7958bb5cc3bfdfdf130d
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2024-12-19 11:58:18 +0530

    Update cov_build.sh

--- cov_build.sh
+++ cov_build.sh
@@ -21,11 +21,7 @@ sed -i '/WDMP_ERR_SESSION_IN_PROGRESS/a\    WDMP_ERR_INTERNAL_ERROR,\n    WDMP_E
 cd $ROOT
 #Build rdk-logger
 git clone https://github.com/rdkcentral/rdk_logger.git
-wget --no-check-certificate https://sourceforge.net/projects/log4c/files/log4c/1.2.4/log4c-1.2.4.tar.gz/download -O log4c-1.2.4.tar.gz
-tar -xvf log4c-1.2.4.tar.gz
-cd log4c-1.2.4
-./configure
-make && make install
+apt install liblog4c-dev
 cd $ROOT/rdk_logger
 autoreconf -i
 ./configure
