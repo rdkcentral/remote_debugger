9381f109c5b9f8fe43df4084a424c451799d86f5
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2024-12-25 07:41:50 +0530

    Update cov_build.sh

--- cov_build.sh
+++ cov_build.sh
@@ -10,24 +10,24 @@ cd rfc
 autoreconf -i
 ./configure --enable-rfctool=yes --enable-tr181set=yes
 cd rfcapi
-cp /usr/local/lib/pkgconfig/libcjson.pc /usr/local/lib/pkgconfig/cjson.pc
 make librfcapi_la_CPPFLAGS="-I/usr/include/cjson -I/usr/rfc/rfcMgr/gtest/mocks"
 make install
 cd /usr/rfc/tr181api
 g++ -fPIC -shared -o libtr181api.so tr181api.cpp -I/usr/local/include/wdmp-c
 mv ./libtr181api.so /usr/local/lib
-mv ./tr181api.h /usr/local/include
-#Build trower-base64
-cd ${ROOT}
-git clone https://github.com/xmidt-org/trower-base64.git
-cd ${ROOT}/trower-base64
-meson setup build
-ninja -C build
-ninja -C build install
+cp ./tr181api.h /usr/local/include
 cd $ROOT
+rm -rf iarmmgrs
+rm -rf iarmbus
 git clone https://github.com/rdkcentral/iarmmgrs.git
+cp /usr/iarmmgrs/rdmmgr/include/rdmMgr.h /usr/local/include
+git clone https://github.com/rdkcentral/iarmbus.git
+cp /usr/iarmbus/core/include/libIBusDaemon.h /usr/local/include
+cp /usr/iarmbus/core/include/libIBus.h /usr/local/include
+cp /usr/iarmbus/core/libIARMCore.h /usr/local/include
+cp /usr/iarmmgrs/hal/include/pwrMgr.h /usr/local/include/
 cd $WORKDIR
 autoreconf -i
 autoupdate
 ./configure --prefix=${INSTALL_DIR}
-make remotedebugger_CFLAGS="-I/usr/include/cjson -I/usr/local/include/wdmp-c -I/usr/local/include/rbus -I/usr/WebconfigFramework/include -I/usr/iarmmgrs/rdmmgr/include -I/usr/iarmmgrs/hal/include -I/usr/local/include/trower-base64" remotedebugger_LDFLAGS="-L/usr/local/lib -lsafec -lrdkloggers -lcjson -lrfcapi -lrbus -lmsgpackc -lsecure_wrapper -lwebconfig_framework -ltr181api  -L/usr/local/lib/x86_64-linux-gnu -ltrower-base64 -L/usr/lib/x86_64-linux-gnu"
+make remotedebugger_CFLAGS="-I/usr/include/cjson -I/usr/local/include/wdmp-c -I/usr/local/include/rbus -I/usr/local/include  -I/usr/WebconfigFramework/include -I/usr/local/include/trower-base64" remotedebugger_LDFLAGS="-L/usr/local/lib -lrdkloggers -lcjson -lrfcapi -lrbus -lmsgpackc -lsecure_wrapper -lwebconfig_framework -lIARMBus -ltr181api  -L/usr/local/lib/x86_64-linux-gnu -ltrower-base64 -L/usr/lib/x86_64-linux-gnu"
