2d86cecd728e1da7f47701e453a63cc384d7f1d4
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2025-01-18 20:44:17 +0530

    Update rrdEventProcess.c

--- src/rrdEventProcess.c
+++ src/rrdEventProcess.c
@@ -20,7 +20,6 @@
 #include "rrdRunCmdThread.h"
 #include "rrdJsonParser.h"
 #include "rrdEventProcess.h"
-#include "rrdCommon.h"
 #define COMMAND_DELIM ';'
 
 static void processIssueType(data_buf *rbuf);
