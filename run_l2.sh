f0ed0b60178b6eb28ea8ec173424b47d220ed1fd
nhanasi <navihansi@gmail.com>
2025-02-07 11:38:17 -0500

    Create run_l2.sh

---
+++ run_l2.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+####################################################################################
+# If not stated otherwise in this file or this component's Licenses.txt file the
+# following copyright and licenses apply:
+#
+# Copyright 2024 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+####################################################################################
+
+RESULT_DIR="/tmp/remotedebugger_test_report"
+OUTPUT_DIR="/tmp/rrd"
+mkdir -p "$RESULT_DIR"
+mkdir -p "$OUTPUT_DIR"
+
+cp remote_debugger.json /etc/rrd/
+cp scripts/uploadRRDL2Logs.sh /lib/rdk/
+rm -rf /opt/logs/remotedebugger.log.0
+
+# Run L2 Test cases
+pytest --json-report --json-report-summary --json-report-file $RESULT_DIR/rrd_single_instance.json test/functional-tests/tests/test_rrd_single_instance.py
+pytest --json-report --json-report-summary --json-report-file $RESULT_DIR/rrd_start_control.json test/functional-tests/tests/test_rrd_start_control.py
+pytest --json-report --json-report-summary --json-report-file $RESULT_DIR/rrd_start_subscribe_and_wait.json test/functional-tests/tests/test_rrd_start_subscribe_and_wait.py
+pytest --json-report --json-report-summary --json-report-file $RESULT_DIR/rrd_static_profile_report.json test/functional-tests/tests/test_rrd_static_profile_report.py
