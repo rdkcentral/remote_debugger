3c19e34da4aa14a09e0088244d71964f907eb4a6
nhanasi <navihansi@gmail.com>
2025-02-19 19:59:58 +0000

    RDK-56099: Pytest L2 Implementation Code Changes
    Correct the duplicate functions and add more logcheck

--- test/functional-tests/tests/test_rrd_single_instance.py
+++ test/functional-tests/tests/test_rrd_single_instance.py
@@ -49,5 +49,6 @@ def test_tear_down():
     run_shell_command(command_to_stop)
     command_to_get_pid = "pidof remotedebugger"
     remove_logfile()
+    remove_outdir_contents(OUTPUT_DIR)
     pid = run_shell_command(command_to_get_pid)
     assert pid == ""
