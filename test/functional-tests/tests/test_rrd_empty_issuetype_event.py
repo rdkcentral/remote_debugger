842bf96c297a68ae1c4a0120066eec8b6e159ea4
nhanasi <navihansi@gmail.com>
2025-02-20 11:39:37 -0500

    rebase (#54)
    * RDK-56099: Pytest L2 Implementation Code Changes (#39)
    
    * Create L2-tests.yml
    
    * Update cov_build.sh
    
    * Update remote_debugger.json
    
    * Create uploadRRDL2Logs.sh
    
    * Update uploadRRDL2Logs.sh
    
    * Create run_l2.sh
    
    * Create rrd_start_control.feature
    
    * Create test_rrd_start_control.py
    
    * Create rrd_single_instance.feature
    
    * Create rrd_start_subscribe_and_wait.feature
    
    * Create test_rrd_start_subscribe_and_wait.py
    
    * Create test_rrd_single_instance.py
    
    * Create helper_functions.py
    
    * Update run_l2.sh
    
    * Update L2-tests.yml
    
    * Update run_l2.sh
    
    * Delete scripts/uploadRRDL2Logs.sh
    
    * Update rrd_start_control.feature
    
    * RDK-56099: Pytest L2 Implementation Code Changes (#42)
    
    * Create L2-tests.yml
    
    * Update cov_build.sh
    
    * Update remote_debugger.json
    
    * Create uploadRRDL2Logs.sh
    
    * Update uploadRRDL2Logs.sh
    
    * Create run_l2.sh
    
    * Create rrd_start_control.feature
    
    * Create test_rrd_start_control.py
    
    * Create rrd_single_instance.feature
    
    * Create rrd_start_subscribe_and_wait.feature
    
    * Create test_rrd_start_subscribe_and_wait.py
    
    * Create test_rrd_single_instance.py
    
    * Create helper_functions.py
    
    * Update run_l2.sh
    
    * Update L2-tests.yml
    
    * Update run_l2.sh
    
    * Delete scripts/uploadRRDL2Logs.sh
    
    * Update rrd_start_control.feature
    
    * RDK-56099: Pytest L2 Implementation Code Changes
    
    Add L2 test cases for following scenarios
    1. corrupted static profile
    2. harmfull command in static profile
    3. Missing issue in static profile
    4. Empty issue rbus event
    5. Static profile report
    6. Static profile Category report
    
    * Update remote_debugger.json
    
    * Create systemd-run
    
    * Update run_l2.sh
    
    * Create rrd_background_cmd_static_profile_report.feature
    
    * Create test_rrd_background_cmd_static_profile_report.py
    
    * Update uploadRRDLogs.sh
    
    * Update helper_functions.py
    
    * Update run_l2.sh
    
    * RDKE-263:[OSCR Scan] RDKE - remote_debugger repo (#44)
    
    * RDKE-263:[OSCR Scan] RDKE - remote_debugger repo
    
    There is a still a reference to the rdk-e repo in src/unittest/README.md. This must be removed.
    
    Many files missing headers: not a blocker but a pity to let this slip. Could the component owner look at this please?
    cov_build.sh
    scripts/systemd-run
    test/functional-tests/tests/*
    
    * Update remote_debugger.json
    
    * Update run_l2.sh
    
    * Create rrd_debug_report_upload.feature
    
    * Create test_rrd_debug_report_upload.py
    
    * Update test_rrd_harmful_command_static_report.py
    
    * Update rrdMain.c
    
    * Update rrdInterface.c
    
    * Update rrd_empty_issuetype_event.feature
    
    * Update test_rrd_empty_issuetype_event.py
    
    * Update test_rrd_debug_report_upload.py
    
    * Update rrd_debug_report_upload.feature
    
    * RDK-56099: Enable L2 test workflows with PR
    
    * RDK-56099: Pre-clone dependent repositories
    
    * Move native builds as optional
    
    Native builds are covered with L2 validations
    
    * RDK-56099: Correct the report directory location
    
    * Update L2-tests.yml
    
    * Update run_l2.sh
    
    * Update test_rrd_debug_report_upload.py
    
    * Create uploadSTBLogs.sh
    
    * Update L2-tests.yml
    
    * Update L2-tests.yml
    
    * RDK-56099: Pytest L2 Implementation Code Changes
    
    Correct the duplicate functions and add more logcheck
    
    * RDK-56099: Pytest L2 Implementation Code Changes
    
    Correct the duplicate functions and add more logcheck
    
    ---------
    
    Co-authored-by: Shibu Kakkoth Vayalambron <mfox@mfox.local>
    Co-authored-by: shibu-kv <shibu.kakkoth@gmail.com>
    Co-authored-by: Venkata Bojja <39968865+venkat0557@users.noreply.github.com>

---
+++ test/functional-tests/tests/test_rrd_empty_issuetype_event.py
@@ -0,0 +1,61 @@
+##########################################################################
+# If not stated otherwise in this file or this component's LICENSE
+# file the following copyright and licenses apply:
+#
+# Copyright 2018 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+
+from time import sleep
+from helper_functions import *
+
+def test_check_remotedebugger_is_starting():
+    kill_rrd()
+    remove_logfile()
+    print("Starting remotedebugger process")
+    command_to_start = "nohup /usr/local/bin/remotedebugger > /dev/null 2>&1 &"
+    run_shell_silent(command_to_start)
+    command_to_get_pid = "pidof remotedebugger"
+    pid = run_shell_command(command_to_get_pid)
+    assert pid != "", "remotedebugger process did not start"
+
+    sleep(2)
+    SUBSCRIBE = "SUCCESS: RBUS Event Subscribe for RRD done!"
+    assert SUBSCRIBE in grep_rrdlogs(SUBSCRIBE)
+
+    sleep(2)
+    EVENT_MSG = "Waiting for for TR69/RBUS Events."
+    assert EVENT_MSG in grep_rrdlogs(EVENT_MSG)
+
+def test_remote_debugger_trigger_event():
+    sleep(10)
+    command = [
+        'rbuscli', 'set',
+        'Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.RDKRemoteDebugger.IssueType',
+        'string', ''
+    ]
+    result = subprocess.run(command, capture_output=True, text=True)
+    assert result.returncode == 0
+
+    sleep(15)
+
+    QUERY_MSG = "Received event for RRD_SET_ISSUE_EVENT"
+    assert QUERY_MSG in grep_rrdlogs(QUERY_MSG)
+
+    EVENT_MSG = "Message Received is empty, Exit Processing"
+    assert EVENT_MSG in grep_rrdlogs(EVENT_MSG)
+
+    remove_outdir_contents(OUTPUT_DIR)
+    remove_logfile()
+    kill_rrd()
