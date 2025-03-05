aceb56de4ab01e2e1a3f22cf6dfe0720c6c7d675
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2025-03-05 23:43:06 +0530

    Update rrdDynamic.c

--- src/rrdDynamic.c
+++ src/rrdDynamic.c
@@ -228,7 +228,7 @@ void RRDRdmManagerDownloadRequest(issueNodeData *pissueStructNode, char *dynJSON
                     RDK_LOG(RDK_LOG_DEBUG, LOG_REMDEBUG, "[%s:%d]: Request RDM Manager Download for... %s\n", __FUNCTION__, __LINE__, paramString);
                     rbusError_t rc = RBUS_ERROR_BUS_ERROR;
                     rbusValue_t value = NULL;
-	            rbusValue_Init(&value);
+	            //rbusValue_Init(&value);
                     rbusValue_SetString(value,paramString);
                     rc = rbus_set(rrdRbusHandle,RDM_MGR_PKG_INST, value, NULL);
 		    //tr181status = setParam("rrd", RDM_MGR_PKG_INST, paramString);
