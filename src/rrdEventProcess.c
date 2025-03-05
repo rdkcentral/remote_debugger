d0de0752c0188d3944bad6c3ed95eeed157d21a1
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2025-03-05 20:41:26 +0530

    Update rrdEventProcess.c

--- src/rrdEventProcess.c
+++ src/rrdEventProcess.c
@@ -337,10 +337,8 @@ issueData* processIssueTypeInDynamicProfileappend(data_buf *rbuf, issueNodeData 
     if (jsonParsed == NULL)
     {
         RDK_LOG(RDK_LOG_ERROR, LOG_REMDEBUG, "[%s:%d]: Dynamic Profile Parse/Read failed... %s\n", __FUNCTION__, __LINE__, dynJSONPath);
-#ifdef IARMBUS_SUPPORT
         RDK_LOG(RDK_LOG_INFO, LOG_REMDEBUG, "[%s:%d]: Going to RDM Request... \n", __FUNCTION__, __LINE__);
         RRDRdmManagerDownloadRequest(pIssueNode, dynJSONPath, rbuf, false); //goto RDM_RRD_REQ_LABEL;
-#endif
     }
     else
     {
@@ -449,10 +447,8 @@ static void processIssueTypeInInstalledPackage(data_buf *rbuf, issueNodeData *pI
     if (jsonParsed == NULL)
     {
         RDK_LOG(RDK_LOG_ERROR, LOG_REMDEBUG, "[%s:%d]: Dynamic Profile Parse/Read failed... %s\n", __FUNCTION__, __LINE__, dynJSONPath);
-#ifdef IARMBUS_SUPPORT
         RDK_LOG(RDK_LOG_INFO, LOG_REMDEBUG, "[%s:%d]: Going to RDM Request... \n", __FUNCTION__, __LINE__);
         RRDRdmManagerDownloadRequest(pIssueNode, dynJSONPath, rbuf, false); //goto RDM_RRD_REQ_LABEL;
-#endif
     }
     else
     {
@@ -468,10 +464,8 @@ static void processIssueTypeInInstalledPackage(data_buf *rbuf, issueNodeData *pI
         }
         else
         {
-#ifdef IARMBUS_SUPPORT
             // Issue Data not in Dynamic Profile JSON
             RRDRdmManagerDownloadRequest(pIssueNode, dynJSONPath, rbuf, false);
-#endif
         }
     }
     freeParsedJson(jsonParsed);
