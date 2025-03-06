9c82eef6012066ae044c49d4e096a7fcf84a558d
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2025-03-06 21:42:57 +0530

    Update rrdDynamic.c

--- src/rrdDynamic.c
+++ src/rrdDynamic.c
@@ -157,6 +157,7 @@ int RRDGetProfileStringLength(issueNodeData *pissueStructNode, bool isDeepSleepA
  * @param bool isDeepSleepAwakeEvent - Flag to indicate if this is a deep sleep awake event.
  * @return void
  */
+#if !defined(GTEST_ENABLE)
 void RRDRdmManagerDownloadRequest(issueNodeData *pissueStructNode, char *dynJSONPath, data_buf *rbuf, bool isDeepSleepAwakeEvent)
 {
     char *paramString = NULL;
@@ -279,7 +280,7 @@ void RRDRdmManagerDownloadRequest(issueNodeData *pissueStructNode, char *dynJSON
     RDK_LOG(RDK_LOG_DEBUG, LOG_REMDEBUG, "[%s:%d]: ...Exiting...\n", __FUNCTION__, __LINE__);
     return;
 }
-
+#endif
 /*
  * @function RRDCheckIssueInDynamicProfile
  * @brief Checks for a specific issue in the dynamic JSON profile associated with the given
