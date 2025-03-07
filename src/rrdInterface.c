104f0c272dc3ccc5a26049936b2bf881e5502906
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2025-03-07 14:34:22 +0530

    Update rrdInterface.c

--- src/rrdInterface.c
+++ src/rrdInterface.c
@@ -218,7 +218,7 @@ void _rdmDownloadEventHandler(rbusHandle_t handle, rbusEvent_t const* event, rbu
     retCode = rbus_get(rrdRbusHandle, RRD_SET_ISSUE_EVENT, &value);
     if(retCode != RBUS_ERROR_SUCCESS)
     {
-         RDK_LOG(RDK_LOG_DEBUG,LOG_REMDEBUG,"[%s:%d]: Rbus get failed for the event [%s]\n", __FUNCTION__, __LINE__, RRD_SET_ISSUE_EVENT));
+         RDK_LOG(RDK_LOG_DEBUG,LOG_REMDEBUG,"[%s:%d]: RBUS get failed for the event [%s]\n", __FUNCTION__, __LINE__, RRD_SET_ISSUE_EVENT);
     }
     RDK_LOG(RDK_LOG_DEBUG,LOG_REMDEBUG,"[%s:%d]: issue type_value: = [%s]\n", __FUNCTION__, __LINE__, rbusValue_GetString(value, NULL));
     issue =rbusValue_GetString(value, NULL);	
