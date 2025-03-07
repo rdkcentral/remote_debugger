03a96eff33327903f8043ece642ca70dd85a6661
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2025-03-07 12:27:18 +0530

    Update rrdJsonParser.c

--- src/rrdJsonParser.c
+++ src/rrdJsonParser.c
@@ -327,6 +327,14 @@ issueData * getIssueCommandInfo(issueNodeData *issuestructNode, cJSON *jsoncfg, 
         check = cJSON_GetObjectItem(sanity, "Check");
         checkval = cJSON_Print(check);  // Print list of sanity commands received
         RDK_LOG(RDK_LOG_DEBUG,LOG_REMDEBUG,"[%s:%d]: Reading Sanity Check Commands List: \n%s\n",__FUNCTION__,__LINE__,checkval);
+	if(!checkval)
+	{
+            jsoncfg = readAndParseJSON(RRD_JSON_FILE);
+	    sanity = cJSON_GetObjectItem(jsoncfg, "Sanity");
+	    check = cJSON_GetObjectItem(sanity, "Check");
+            checkval = cJSON_Print(check);  // Print list of sanity commands received
+            RDK_LOG(RDK_LOG_DEBUG,LOG_REMDEBUG,"[%s:%d]: Reading Sanity Check Commands List: \n%s\n",__FUNCTION__,__LINE__,checkval);
+        }
         cmdlist = cJSON_GetObjectItem(check, "Commands");
         cJSON_free(checkval); // free sanity command list
         ret = isCommandsValid(issuestdata->command, cmdlist);
