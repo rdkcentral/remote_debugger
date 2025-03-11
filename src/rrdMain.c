508fc963b5eb7cddca3a01b1c73b4ede400e63ab
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2025-03-11 11:34:36 +0530

    Update rrdMain.c

--- src/rrdMain.c
+++ src/rrdMain.c
@@ -49,7 +49,7 @@ void *RRDEventThreadFunc(void *arg)
         }
         rbuf = (data_buf *)msgHdr.mbody;
         RDK_LOG(RDK_LOG_DEBUG, LOG_REMDEBUG, "[%s:%d]:SUCCESS: Message Reception Done for ID=%d MSG=%s TYPE=%u... \n", __FUNCTION__, __LINE__, msqid, rbuf->mdata, rbuf->mtype);
-#if !defined(GTEST_ENABLE) 
+//#if !defined(GTEST_ENABLE) 
 	if (rbuf->mdata[0] == '\0')
 	{
             RDK_LOG(RDK_LOG_DEBUG, LOG_REMDEBUG, "[%s:%d]:Message Received is empty, Exit Processing!!! \n", __FUNCTION__, __LINE__);      
@@ -58,7 +58,7 @@ void *RRDEventThreadFunc(void *arg)
         else {
             RDK_LOG(RDK_LOG_DEBUG, LOG_REMDEBUG, "[%s:%d]:SUCCESS: Message Reception Done for ID=%d MSG=%s TYPE=%u... \n", __FUNCTION__, __LINE__, msqid, rbuf->mdata, rbuf->mtype);
         }
-#endif
+//#endif
         switch (rbuf->mtype)
         {
         case EVENT_MSG:
