eb467af8520f9e407166e44842dd556e9492490a
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2025-03-27 14:07:01 +0530

    Create test_rrd_dynamic_profile_harmful_report.py

---
+++ test/functional-tests/tests/test_rrd_dynamic_profile_harmful_report.py
@@ -0,0 +1,87 @@
+import json
+from helper_functions import *
+
+def test_check_remote_debugger_config_file():
+    config_file_path = JSON_FILE
+    assert check_file_exists(config_file_path), f"Configuration file '{config_file_path}' does not exist."
+
+def test_check_rrd_directory_exists():
+    dir_path = OUTPUT_DIR
+    assert check_directory_exists(dir_path), f"Directory '{dir_path}' does not exist."
+
+def test_check_and_start_remotedebugger():
+    kill_rrd()
+    remove_logfile()
+    print("Starting remotedebugger process")
+    command_to_start = "nohup /usr/local/bin/remotedebugger > /dev/null 2>&1 &"
+    run_shell_silent(command_to_start)
+    command_to_get_pid = "pidof remotedebugger"
+    pid = run_shell_command(command_to_get_pid)
+    assert pid != "", "remotedebugger process did not start"
+
+def reset_issuetype_rfc():
+    command = 'rbuscli set Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.RDKRemoteDebugger.IssueType string ""'
+    result = subprocess.run(command, shell=True, capture_output=True, text=True)
+    assert result.returncode == 0
+
+def test_remote_debugger_trigger_event():
+    reset_issuetype_rfc()
+    sleep(10)
+    command = [
+        'rbuscli', 'set',
+        'Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.RDKRemoteDebugger.IssueType',
+        'string', DYNAMIC_HARMFUL_STRING
+    ]
+    result = subprocess.run(command, capture_output=True, text=True)
+    assert result.returncode == 0
+
+    sleep(15)
+
+    QUERY_MSG = "Received event for RRD_SET_ISSUE_EVENT"
+    assert QUERY_MSG in grep_rrdlogs(QUERY_MSG)
+
+    MSG_SEND = "SUCCESS: Message sending Done"
+    sleep(2)
+    assert MSG_SEND in grep_rrdlogs(MSG_SEND)
+
+    MSG_RECEIVE = "SUCCESS: Message Reception Done"
+    sleep(2)
+    assert MSG_RECEIVE in grep_rrdlogs(MSG_RECEIVE)
+
+def test_check_issue_in_static_profile():
+    READ_JSON = "Start Reading JSON File... /etc/rrd/remote_debugger.json"
+    assert READ_JSON in grep_rrdlogs(READ_JSON)
+
+    PARSE_JSON = "Static Profile Parse And Read Success"
+    assert PARSE_JSON in grep_rrdlogs(PARSE_JSON)
+
+    MISSING_MSG = "Issue Data Not found in Static JSON File"
+    assert MISSING_MSG in grep_rrdlogs(MISSING_MSG)
+
+def test_check_issue_in_dynamic_profile():
+    DYNAMIC_READ = "Checking Dynamic Profile..."
+    assert DYNAMIC_READ in grep_rrdlogs(DYNAMIC_READ)
+
+    DYNAMIC_JSONFILE = "Reading json config file /media/apps/RDK-RRD-Test/etc/rrd/remote_debugger.json"
+    assert DYNAMIC_JSONFILE in grep_rrdlogs(DYNAMIC_JSONFILE)
+
+    PARSE_SUCCESS = "Dynamic Profile Parse And Read Success..."
+
+
+def test_remote_debugger_command_sanity():
+    VALID_COMMAND = "rm -rf"
+    assert VALID_COMMAND in grep_rrdlogs(VALID_COMMAND)
+
+    SANITY_CHECK = "Found harmful commands"
+    assert SANITY_CHECK in grep_rrdlogs(SANITY_CHECK)
+
+    sleep(5)
+    ABORT_MSG = "Aborting Command execution due to Harmful commands"
+    assert ABORT_MSG in grep_rrdlogs(ABORT_MSG)
+
+    UPLOAD_MSG = "Skip uploading Debug Report"
+    assert UPLOAD_MSG in grep_rrdlogs(UPLOAD_MSG)
+
+    #remove_logfile()
+    remove_outdir_contents(OUTPUT_DIR)
+    kill_rrd()
