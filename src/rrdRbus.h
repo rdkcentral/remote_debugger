f2ebc018524228df2712a273b8fc30f3530b21eb
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2025-04-07 13:58:21 +0530

    Update rrdRbus.h

--- src/rrdRbus.h
+++ src/rrdRbus.h
@@ -28,7 +28,7 @@ extern "C"
 #if !defined(GTEST_ENABLE)
 #include <rbus.h>
 
-rbusEventSubscription_t subscriptions[4];
+rbusEventSubscription_t subscriptions[3];
 #endif
 
 void webconfigFrameworkInit();
