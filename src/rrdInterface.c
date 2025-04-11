f8661e4e3e397d5d73b964857d81e5ab1ba3cf13
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2025-04-11 13:26:30 +0530

    Update rrdInterface.c

--- src/rrdInterface.c
+++ src/rrdInterface.c
@@ -81,13 +81,13 @@ int RRD_subscribe()
    subscriptions[2].handler  = _rdmDownloadEventHandler;
    subscriptions[2].userData = NULL;
 #else
-   subscriptions[3].eventName = RDM_DOWNLOAD_EVENT1;
-   subscriptions[3].filter = NULL;
-   subscriptions[3].duration = 0;
-   subscriptions[3].handler  = _rdmDownloadEventHandler;
-   subscriptions[3].userData = NULL;
+   subscriptions[2].eventName = RDM_DOWNLOAD_EVENT1;
+   subscriptions[2].filter = NULL;
+   subscriptions[2].duration = 0;
+   subscriptions[2].handler  = _rdmDownloadEventHandler;
+   subscriptions[2].userData = NULL;
 #endif
-   ret = rbusEvent_SubscribeEx(rrdRbusHandle, subscriptions, 4, 60);
+   ret = rbusEvent_SubscribeEx(rrdRbusHandle, subscriptions, 3, 60);
 //#else
    //ret = rbusEvent_SubscribeEx(rrdRbusHandle, subscriptions, 2, 60);
 //#endif
