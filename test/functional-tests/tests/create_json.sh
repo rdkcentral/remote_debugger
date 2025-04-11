ca78c0262d6fc9f72471d0d52f54ba70ce0a29f4
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2025-04-11 21:45:10 +0530

    Update create_json.sh

--- test/functional-tests/tests/create_json.sh
+++ test/functional-tests/tests/create_json.sh
@@ -1,28 +1,15 @@
-##########################################################################
-# If not stated otherwise in this file or this component's LICENSE
-# file the following copyright and licenses apply:
-#
-# Copyright 2018 RDK Management
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-##########################################################################
-
 #!/bin/bash
 
 # Step 1: Create a JSON file
 json_file="/media/apps/RDK-RRD-Test/etc/rrd/remote_debugger.json"
 echo '{
   "Test": {
+    "TestRun5": {
+      "Commands": "cat /version.txt;uptime;cat /tmp/.deviceDetails.cache",
+      "Timeout": 10                                                                                                                      },
+    "TestRun4": {
+      "Commands": "cat /version.txt;uptime;cat /tmp/.deviceDetails.cache",
+      "Timeout": 10                                                                                                                      },
     "TestRun3": {
       "Commands": "cat /version.txt;uptime;rm -rf;cat /tmp/.deviceDetails.cache",
       "Timeout": 10
@@ -37,3 +24,5 @@ echo '{
     }
   }
 }' > $json_file
+mkdir -p /tmp/RDK-RRD-Test/etc/rrd/
+cp $json_file /tmp/RDK-RRD-Test/etc/rrd/
