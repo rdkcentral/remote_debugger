64e92cb5ef5208f5dba5caddcdfb4e6ae2d3e1cb
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2025-04-16 12:45:33 +0530

    Update test_append.py

--- test/functional-tests/tests/test_append.py
+++ test/functional-tests/tests/test_append.py
@@ -1,25 +1,29 @@
-##########################################################################
-# If not stated otherwise in this file or this component's LICENSE
-# file the following copyright and licenses apply:
-#
-# Copyright 2018 RDK Management
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-##########################################################################
-
 import json
-import subprocess
 from helper_functions import *
+
+# Path to the existing JSON file
+file_path = "/etc/rrd/remote_debugger.json"
+
+# Read the existing JSON data
+with open(file_path, "r") as json_file:
+    data = json.load(json_file)
+
+# New entry to add
+new_entry = {
+    "Test": {
+        "TestRun4": {
+            "Commands": "cat /version.txt;cat /tmp/.deviceDetails.cache",
+            "Timeout": 10
+        }
+    }
+}
+
+# Update the JSON data with the new entry
+data.update(new_entry)
+
+# Write the updated data back to the JSON file
+with open(file_path, "w") as json_file:
+    json.dump(data, json_file, indent=4)
 
 def test_check_remote_debugger_config_file():
     config_file_path = JSON_FILE
@@ -29,9 +33,19 @@ def test_check_rrd_directory_exists():
     dir_path = OUTPUT_DIR
     assert check_directory_exists(dir_path), f"Directory '{dir_path}' does not exist."
 
+def test_check_dynamic_config_file():
+    config_file_path = APPEND_JSON_FILE
+    assert check_file_exists(config_file_path), f"Configuration file '{config_file_path}' does not exist."
+
+def test_check_dynamic_directory_exists():
+    dir_path = DYNAMIC_DIR
+    assert check_directory_exists(dir_path), f"Directory '{dir_path}' does not exist."
+
 def test_check_and_start_remotedebugger():
     kill_rrd()
     remove_logfile()
+    test_check_dynamic_directory_exists()
+    test_check_dynamic_config_file()
     print("Starting remotedebugger process")
     command_to_start = "nohup /usr/local/bin/remotedebugger > /dev/null 2>&1 &"
     run_shell_silent(command_to_start)
@@ -45,13 +59,13 @@ def reset_issuetype_rfc():
     assert result.returncode == 0
 
 def test_remote_debugger_trigger_event():
-    MISSING_STRING_NEG = "Test.TestRun7"
+    APPEND_STRING1 = "Test.TestRun4_apnd"
     reset_issuetype_rfc()
     sleep(10)
     command = [
         'rbuscli', 'set',
         'Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.RDKRemoteDebugger.IssueType',
-        'string', MISSING_STRING_NEG
+        'string', APPEND_STRING1
     ]
     result = subprocess.run(command, capture_output=True, text=True)
     assert result.returncode == 0
@@ -69,90 +83,51 @@ def test_remote_debugger_trigger_event():
     sleep(2)
     assert MSG_RECEIVE in grep_rrdlogs(MSG_RECEIVE)
 
-def test_check_issue_in_static_profile():
-    READ_JSON = "Start Reading JSON File... /etc/rrd/remote_debugger.json"
-    assert READ_JSON in grep_rrdlogs(READ_JSON)
-
-    PARSE_JSON = "Static Profile Parse And Read Success"
-    assert PARSE_JSON in grep_rrdlogs(PARSE_JSON)
-
-    MISSING_MSG = "Issue Data Not found in Static JSON File"
-    assert MISSING_MSG in grep_rrdlogs(MISSING_MSG)
-
 def test_check_issue_in_dynamic_profile():
-    DYNAMIC_READ = "Checking Dynamic Profile..."
-    assert DYNAMIC_READ in grep_rrdlogs(DYNAMIC_READ)
+    APPEND_MSG = "Received append request to process static and dynamic profiles"
+    assert APPEND_MSG in grep_rrdlogs(APPEND_MSG)
 
     DYNAMIC_JSONFILE = "Reading json config file /media/apps/RDK-RRD-Test/etc/rrd/remote_debugger.json"
     assert DYNAMIC_JSONFILE in grep_rrdlogs(DYNAMIC_JSONFILE)
 
-    PARSE_FAILED = "Dynamic Profile Parse/Read failed"
-    assert PARSE_FAILED in grep_rrdlogs(PARSE_FAILED)
+    JSON_READ_SUCCESS = "Reading json file Success"
+    assert JSON_READ_SUCCESS in grep_rrdlogs(JSON_READ_SUCCESS)
 
-    RDM_MSG = "Request RDM Manager Download for a new Issue Type"
-    assert RDM_MSG in grep_rrdlogs(RDM_MSG)
-
-    RDM_PACKAGE = "Request RDM Manager Download for... RDK-RRD-Test:1.0"
-    assert RDM_PACKAGE in grep_rrdlogs(RDM_PACKAGE)
-
-    script_path="./test/functional-tests/tests/create_json.sh"
-    # Run the shell script
-    try:
-        result = subprocess.run(['bash', script_path], check=True, text=True, capture_output=True)
-        print("Script output:")
-        print(result.stdout)
-    except subprocess.CalledProcessError as e:
-        print("Error while executing the script:")
-        print(e.stderr)
-
-def test_rdm_trigger_event():
-    INSTALL_PACKAGE ="RDK-RRD-Test:1.0"
-    command = [
-        'rbuscli', 'set',
-        'Device.DeviceInfo.X_RDKCENTRAL-COM_RDKDownloadManager.InstallPackage',
-        'string', INSTALL_PACKAGE
-    ]
-    result = subprocess.run(command, capture_output=True, text=True)
-    assert result.returncode == 0
-
-    RBUS_SETTING_SUCCESS = "Setting Parameters using rbus success..."
-    assert RBUS_SETTING_SUCCESS in grep_rrdlogs(RBUS_SETTING_SUCCESS)
-
-    CACHE_UPDATE_SUCCESS = "Setting Parameters Success and Cache Updated ...RDK-RRD-Test"
-    assert CACHE_UPDATE_SUCCESS in grep_rrdlogs(CACHE_UPDATE_SUCCESS)
-
-def test_rdm_download_event():
-    command = [
-        'rbuscli', 'set',
-        'Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.RDKRemoteDebugger.DownloadStatus',
-        'bool', 'true'
-    ]
-    result = subprocess.run(command, capture_output=True, text=True)
-    assert result.returncode == 0
-
-    RDM_DOWNLOAD_EVENT = "Received event for RDM_DOWNLOAD_EVENT Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.RDKRemoteDebugger.DownloadStatus"
-    assert RDM_DOWNLOAD_EVENT in grep_rrdlogs(RDM_DOWNLOAD_EVENT)
-
-    PACKAGE_FOUND = "Package found in Cache...Test.TestRun7"
-    assert PACKAGE_FOUND in grep_rrdlogs(PACKAGE_FOUND)
-
-    MESSAGE_SUCCESS = "SUCCESS: Message sending Done, ID=0 MSG=Test.TestRun7 Size=13 Type=1 AppendMode=0!"
-    assert MESSAGE_SUCCESS in grep_rrdlogs(MESSAGE_SUCCESS)
-
-    ISSUE_TYPE_LIST = "IssueType List [Test.TestRun7]..."
-    assert ISSUE_TYPE_LIST in grep_rrdlogs(ISSUE_TYPE_LIST)
-
-    CHECKING_DYNAMIC = "Checking if Issue marked inDynamic..."
-    assert CHECKING_DYNAMIC in grep_rrdlogs(CHECKING_DYNAMIC)
-
-    READING_JSON_CONFIG = "Reading json config file /tmp/RDK-RRD-Test/etc/rrd/remote_debugger.json"
-    assert READING_JSON_CONFIG in grep_rrdlogs(READING_JSON_CONFIG)
-
-    JSON_PARSE_SUCCESS = "Reading json file Success, Parsing the Content..."
+    JSON_PARSE_SUCCESS = "Json File parse Success"
     assert JSON_PARSE_SUCCESS in grep_rrdlogs(JSON_PARSE_SUCCESS)
 
-    DYNAMIC_JSON_PARSE_FAILED = "Dynamic JSON Parse/Read failed... /tmp/RDK-RRD-Test/etc/rrd/remote_debugger.json"
-    assert DYNAMIC_JSON_PARSE_FAILED in grep_rrdlogs(DYNAMIC_JSON_PARSE_FAILED)
+    JSON_SUCCESS = "Dynamic Profile Parse And Read Success... /media/apps/RDK-RRD-Test/etc/rrd/remote_debugger.json"
+    assert JSON_SUCCESS in grep_rrdlogs(JSON_SUCCESS)
+
+    CHECKING_DYNAMIC_JSON = "Check if Issue in Parsed Dynamic JSON... /media/apps/RDK-RRD-Test/etc/rrd/remote_debugger.json"
+    assert CHECKING_DYNAMIC_JSON in grep_rrdlogs(CHECKING_DYNAMIC_JSON)
+
+    STATIC_READ = "Reading static profile command info..."
+    assert STATIC_READ in grep_rrdlogs(STATIC_READ)
+
+    READING_JSON = "Start Reading JSON File... /etc/rrd/remote_debugger.json"
+    assert READING_JSON in grep_rrdlogs(READING_JSON)
+
+    JSON_STATIC_SUCCESS = "Reading json file Success, Parsing the Content..."
+    assert JSON_STATIC_SUCCESS in grep_rrdlogs(JSON_STATIC_SUCCESS)
+
+    JSON_PARSE_STATIC_SUCCESS = "Json File parse Success... /etc/rrd/remote_debugger.json"
+    assert JSON_PARSE_STATIC_SUCCESS in grep_rrdlogs(JSON_PARSE_STATIC_SUCCESS)
+
+    SUCCESS_STATIC = "Static Profile Parse And Read Success... /etc/rrd/remote_debugger.json"
+    assert SUCCESS_STATIC in  grep_rrdlogs(SUCCESS_STATIC)
+
+    CHECKING_STATIC_JSON = "Check if Issue in Parsed Static JSON... /etc/rrd/remote_debugger.json"
+    assert CHECKING_STATIC_JSON in grep_rrdlogs(CHECKING_STATIC_JSON)
+
+    READ_COMPLETE_STATIC = "Read complete for Static Profile: RFCValue: Test.TestRun4, Command: "
+    assert READ_COMPLETE_STATIC in grep_rrdlogs(READ_COMPLETE_STATIC)
+
+    APPEND_UPDATE = "Updated command after append from dynamic and static profile: "
+    assert APPEND_UPDATE in grep_rrdlogs(APPEND_UPDATE)
+
+    EXECUTE_SERVICE = "Executing Commands in Runtime Service..."
+    assert EXECUTE_SERVICE in grep_rrdlogs(EXECUTE_SERVICE)
 
     remove_logfile()
     remove_outdir_contents(OUTPUT_DIR)
