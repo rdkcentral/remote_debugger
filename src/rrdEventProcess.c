8561d0027c3f680415b9ae462da1eaf09d60c29d
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2025-05-08 20:31:52 +0530

    Update rrdEventProcess.c

--- src/rrdEventProcess.c
+++ src/rrdEventProcess.c
@@ -97,15 +97,30 @@ void processIssueTypeEvent(data_buf *rbuf)
                     {
                         RDK_LOG(RDK_LOG_DEBUG, LOG_REMDEBUG, "[%s:%d]: Memory Allocation Failed... \n", __FUNCTION__, __LINE__);
                     }
-                    free(cmdBuff);
+		    if(cmdBuff)
+		    {
+                        free(cmdBuff);
+			cmdBuff = NULL;
+		    }
+		  RDK_LOG(RDK_LOG_DEBUG, LOG_REMDEBUG, "[%s:%d]: Reaching after free cmdbuff \n", __FUNCTION__, __LINE__);
                 }
                 else
                 {
                     RDK_LOG(RDK_LOG_DEBUG, LOG_REMDEBUG, "[%s:%d]: Memory Allocation Failed... \n", __FUNCTION__, __LINE__);
                 }
-                free(cmdMap[index]);
+		if( cmdMap[index])
+		{
+                    free(cmdMap[index]);
+		    cmdMap[index] = NULL;
+		}
+		RDK_LOG(RDK_LOG_DEBUG, LOG_REMDEBUG, "[%s:%d]: Reaching after free cmdMap[index] \n", __FUNCTION__, __LINE__);
             }
-            free(cmdMap);
+	    if( cmdMap)
+	    {
+                free(cmdMap);
+		cmdMap = NULL;    
+	    }
+	    RDK_LOG(RDK_LOG_DEBUG, LOG_REMDEBUG, "[%s:%d]: Reaching after free cmdMap \n", __FUNCTION__, __LINE__);
         }
     }
     
