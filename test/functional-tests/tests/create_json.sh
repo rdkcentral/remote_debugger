a682fb78cb15f4d2d838fb5ae92d97d7af6cd96f
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2025-05-26 15:00:40 +0530

    Update create_json.sh

--- test/functional-tests/tests/create_json.sh
+++ test/functional-tests/tests/create_json.sh
@@ -20,20 +20,31 @@
 #!/bin/bash
 
 # Step 1: Create a JSON file
+mkdir -p /media/apps/RDK-RRD-Test/etc/rrd/
 json_file="/media/apps/RDK-RRD-Test/etc/rrd/remote_debugger.json"
 echo '{
   "Test": {
+    "TestRun1": {
+      "Commands": "cat /version.txt;uptime;cat /proc/buddyinfo;cat /proc/meminfo;cat /tmp/.deviceDetails.cache",
+      "Timeout": 10
+                },
+    "TestRun5": {
+      "Commands": "cat /version.txt;uptime;cat /tmp/.deviceDetails.cache",
+      "Timeout": 10                                                                                                                  
+                },
+    "TestRun4": {
+      "Commands": "cat /version.txt;uptime;cat /tmp/.deviceDetails.cache",
+      "Timeout": 10                                                                                                                  
+                },
     "TestRun3": {
       "Commands": "cat /version.txt;uptime;rm -rf;cat /tmp/.deviceDetails.cache",
       "Timeout": 10
-    },
+                },
     "TestRun2": {
       "Commands": "cat /version.txt;uptime;/proc/version;cat /proc/buddyinfo;cat /proc/meminfo;cat /tmp/.deviceDetails.cache",
-      "Timeout": 10
-    },
-    "TestRun1": {
-      "Commands": "cat /version.txt;uptime;cat /proc/buddyinfo;cat /proc/meminfo;cat /tmp/.deviceDetails.cache",
       "Timeout": 10
     }
   }
 }' > $json_file
+mkdir -p /tmp/RDK-RRD-Test/etc/rrd/
+cp $json_file /tmp/RDK-RRD-Test/etc/rrd/
