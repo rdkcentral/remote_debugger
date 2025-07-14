2974252dd6f37b6f47b2133b23c8bfb8bb0f5d68
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2025-07-14 13:32:40 +0530

    Update rrdInterface.h

--- src/rrdInterface.h
+++ src/rrdInterface.h
@@ -60,10 +60,10 @@ void _remoteDebuggerEventHandler(rbusHandle_t handle, rbusEvent_t const* event, 
 void _remoteDebuggerWebCfgDataEventHandler(rbusHandle_t handle, rbusEvent_t const* event, rbusEventSubscription_t* subscription);
 void _rdmDownloadEventHandler(rbusHandle_t handle, rbusEvent_t const* event, rbusEventSubscription_t* subscription);
 #endif
-
+//#ifdef IARMBUS_SUPPORT
 int RRD_IARM_subscribe(void);
 int RRD_IARM_unsubscribe(void);
-#ifdef IARMBUS_SUPPORT
+
 void _rdmManagerEventHandler(const char *owner, IARM_EventId_t eventId, void *data, size_t len);
 #if defined(PWRMGR_PLUGIN)
 void _pwrManagerEventHandler(const PowerController_PowerState_t currentState,
@@ -71,7 +71,7 @@ void _pwrManagerEventHandler(const PowerController_PowerState_t currentState,
 #else
 void _pwrManagerEventHandler(const char *owner, IARM_EventId_t eventId, void *data, size_t len);
 #endif
-#endif
+//#endif
 void RRD_data_buff_deAlloc(data_buf *sbuf);
 void RRDMsgDeliver(int msgqid, data_buf *sbuf);
 int RRD_subscribe(void);
