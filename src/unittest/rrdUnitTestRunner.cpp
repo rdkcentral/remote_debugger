3c457fd3ff846698b8eb160003c636a8af240a68
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2025-07-15 14:44:57 +0530

    Update rrdUnitTestRunner.cpp

--- src/unittest/rrdUnitTestRunner.cpp
+++ src/unittest/rrdUnitTestRunner.cpp
@@ -4665,7 +4665,7 @@ TEST_F(RRDSubscribeTest, TestRRD_Subscribe_RRDHandlerFail)
     EXPECT_CALL(mock, IARM_Bus_Init(RDK_REMOTE_DEBUGGER_NAME)).WillOnce(::testing::Return(IARM_RESULT_SUCCESS));
     EXPECT_CALL(mock, IARM_Bus_Connect()).WillOnce(::testing::Return(IARM_RESULT_SUCCESS));
     EXPECT_CALL(mock, IARM_Bus_RegisterEventHandler(RDK_REMOTE_DEBUGGER_NAME, IARM_BUS_RDK_REMOTE_DEBUGGER_ISSUETYPE, ::testing::_)).WillOnce(::testing::Return(IARM_RESULT_FAILURE));
-    IARM_Result_t result = RRD_IARM_subscribe();
+    int result = RRD_IARM_subscribe();
 
     //EXPECT_NE(result, IARM_RESULT_SUCCESS);
 }
