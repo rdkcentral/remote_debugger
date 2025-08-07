85087e28f5cd06a4401a1b317f4919715cbd52e5
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2025-08-07 20:16:34 +0530

    Update rrdMain.c

--- src/rrdMain.c
+++ src/rrdMain.c
@@ -158,16 +158,6 @@ int main(int argc, char *argv[])
 #endif
     /* Initialize Cache */
     initCache();
-
-    /* Check RRD Enable RFC */
-    bool isEnabled = isRRDEnabled();
-    if(!isEnabled) {
-        RDK_LOG(RDK_LOG_DEBUG,LOG_REMDEBUG,"[%s:%d]:RFC is disabled, stopping remote-debugger\n", __FUNCTION__, __LINE__);
-        exit(0);
-    }
-    
-    RDK_LOG(RDK_LOG_DEBUG,LOG_REMDEBUG,"[%s:%d]:Starting RDK Remote Debugger Daemon \n",__FUNCTION__,__LINE__);
-
     const char *filePath = "/tmp/rrd_enabled";
     FILE *fp = fopen(filePath, "w");
     if (fp) {
@@ -177,6 +167,15 @@ int main(int argc, char *argv[])
     else {
         RDK_LOG(RDK_LOG_ERROR, LOG_REMDEBUG, "[%s:%d]:Failed to touch file %s\n", __FUNCTION__, __LINE__, filePath);
     }
+    /* Check RRD Enable RFC */
+    bool isEnabled = isRRDEnabled();
+    if(!isEnabled) {
+        RDK_LOG(RDK_LOG_DEBUG,LOG_REMDEBUG,"[%s:%d]:RFC is disabled, stopping remote-debugger\n", __FUNCTION__, __LINE__);
+        exit(0);
+    }
+    
+    RDK_LOG(RDK_LOG_DEBUG,LOG_REMDEBUG,"[%s:%d]:Starting RDK Remote Debugger Daemon \n",__FUNCTION__,__LINE__);
+
     if ((msqid = msgget(key, IPC_CREAT | 0666 )) < 0)
     {
         RDK_LOG(RDK_LOG_ERROR,LOG_REMDEBUG,"[%s:%d]:Message Queue ID Creation failed, msqid=%d!!!\n",__FUNCTION__,__LINE__,msqid);
