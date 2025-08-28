6b349ae4606165521facf8a9e9c860b14fd37c69
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2025-08-28 21:29:36 +0530

    Update rrdInterface.c

--- src/rrdInterface.c
+++ src/rrdInterface.c
@@ -74,24 +74,16 @@ int RRD_subscribe()
     subscriptions[1].handler  = _remoteDebuggerWebCfgDataEventHandler;
     subscriptions[1].userData = NULL;
 
-#ifdef IARMBUS_SUPPORT
-#ifdef USE_L2_SUPPORT
-   subscriptions[2].eventName = RDM_DOWNLOAD_EVENT;
-   subscriptions[2].filter = NULL;
-   subscriptions[2].duration = 0;
-   subscriptions[2].handler  = _rdmDownloadEventHandler;
-   subscriptions[2].userData = NULL;
-   ret = rbusEvent_SubscribeEx(rrdRbusHandle, subscriptions, 3, 60);
+#ifndef IARMBUS_SUPPORT
+    subscriptions[2].eventName = RDM_DOWNLOAD_EVENT;
+    subscriptions[2].filter = NULL;
+    subscriptions[2].duration = 0;
+    subscriptions[2].handler  = _rdmDownloadEventHandler;
+    subscriptions[2].userData = NULL;
+
+    ret = rbusEvent_SubscribeEx(rrdRbusHandle, subscriptions, 3, 60);
 #else
-   ret = rbusEvent_SubscribeEx(rrdRbusHandle, subscriptions, 2, 60);
-#endif
-#else
-   subscriptions[2].eventName = RDM_DOWNLOAD_EVENT;
-   subscriptions[2].filter = NULL;
-   subscriptions[2].duration = 0;
-   subscriptions[2].handler  = _rdmDownloadEventHandler;
-   subscriptions[2].userData = NULL;
-   ret = rbusEvent_SubscribeEx(rrdRbusHandle, subscriptions, 3, 60);
+    ret = rbusEvent_SubscribeEx(rrdRbusHandle, subscriptions, 2, 60);
 #endif
 #endif
     if(ret != 0)
