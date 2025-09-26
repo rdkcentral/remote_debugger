4d5315f2744585460f1b82b0c99f6f92ffb076cf
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2025-09-26 19:45:28 +0530

    Update run_l2.sh

--- run_l2.sh
+++ run_l2.sh
@@ -68,10 +68,10 @@ pytest --json-report --json-report-summary --json-report-file $RESULT_DIR/rrd_dy
 rm -rf /media/apps/RDK-RRD-Test/etc/rrd/remote_debugger.json
 rm -rf /tmp/RDK-RRD-Test/etc/rrd/remote_debugger.json
 pytest --json-report --json-report-summary --json-report-file $RESULT_DIR/rrd_dynamic_profile_harmful_with_download.json test/functional-tests/tests/test_rrd_dynamic_with_download_harmful.py
+pytest --json-report --json-report-summary --json-report-file $RESULT_DIR/test_category.json test/functional-tests/tests/test_rrd_dynamic_subcategory_report.py
 rm -rf /media/apps/RDK-RRD-Test/etc/rrd/remote_debugger.json
 rm -rf /tmp/RDK-RRD-Test/etc/rrd/remote_debugger.json
 pytest --json-report --json-report-summary --json-report-file $RESULT_DIR/test_category.json test/functional-tests/tests/test_rrd_dynamic_category_report.py‎
-pytest --json-report --json-report-summary --json-report-file $RESULT_DIR/test_category.json test/functional-tests/tests/test_rrd_dynamic_subcategory_report.py
 pytest --json-report --json-report-summary --json-report-file $RESULT_DIR/rrd_append.json test/functional-tests/tests/test_rrd_append_report.py
 pytest --json-report --json-report-summary --json-report-file $RESULT_DIR/rrd_dynamic_profile_harmful_report.json test/functional-tests/tests/test_rrd_dynamic_profile_harmful_report.py
 cp remote_debugger.json /etc/rrd/
