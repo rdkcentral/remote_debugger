fcafed3b0a78569671d3ebc8a238d638375f8179
Vismalskumar0 <ajvismal@yahoo.com>
2025-12-16 15:00:06 +0530

    RDK-58172: add rrd_upload_cleanup prototype to rrd_upload.h

--- src/rrd_upload.h
+++ src/rrd_upload.h
@@ -11,5 +11,6 @@ int rrd_upload_check_lock(bool *is_locked);
 int rrd_upload_wait_for_lock(int max_attempts, int wait_seconds);
 int rrd_upload_invoke_logupload_api(const char *log_server, const char *protocol, const char *http_link, const char *archive_filename);
 int rrd_upload_cleanup_files(const char *archive_path, const char *source_dir);
+void rrd_upload_cleanup(void);
 
 #endif // RRD_UPLOAD_H
