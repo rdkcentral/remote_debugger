23e4dce5a0b62c7fffb9009a390c22c54d8c15d5
Vismal S Kumar <ajvismal@yahoo.com>
2025-12-23 19:52:58 +0530

    Update rrd_upload.h

--- src/rrd_upload.h
+++ src/rrd_upload.h
@@ -10,7 +10,9 @@
 #include <uploadstblogs.h>
 #endif
 
-
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 int rrd_upload_execute(const char *log_server, const char *protocol, const char *http_link, const char *working_dir, const char *archive_filename);
 int rrd_upload_check_lock(bool *is_locked);
@@ -18,5 +20,9 @@ int rrd_upload_wait_for_lock(int max_attempts, int wait_seconds);
 int rrd_upload_invoke_logupload_api(const char *log_server, const char *protocol, const char *http_link, const char *archive_filename);
 int rrd_upload_cleanup_files(const char *archive_path, const char *source_dir);
 void rrd_upload_cleanup(void);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // RRD_UPLOAD_H
