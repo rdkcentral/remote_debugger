055407081ff01619771bd340af524e5222e0e7f2
Vismal S Kumar <ajvismal@yahoo.com>
2026-01-12 18:12:51 +0530

    Update rrd_archive.c

--- src/rrd_archive.c
+++ src/rrd_archive.c
@@ -1,12 +1,21 @@
 /*
- * rrd_archive.c - Archive Manager (tar writer + gzip via zlib)
+ * If not stated otherwise in this file or this component's LICENSE file the
+ * following copyright and licenses apply:
  *
- * This implementation replaces shell `tar -czf` invocation with an
- * internal tar writer that streams POSIX tar headers and file data into
- * a gzip stream using zlib (`gz*` API). It supports recursive directory
- * traversal, regular files and directories. Symlinks and special devices
- * are not fully supported in this minimal implementation.
- */
+ * Copyright 2018 RDK Management
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
 
 #include "rrd_archive.h"
 
