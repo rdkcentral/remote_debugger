c3b302b1a598a570f3cd18f96b16840170765ca2
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2026-05-03 19:19:05 +0530

    Update rrdJsonParser.h

--- src/rrdJsonParser.h
+++ src/rrdJsonParser.h
@@ -47,6 +47,12 @@ issueData* getIssueCommandInfo(issueNodeData *issuestructNode, cJSON *jsoncfg,ch
 bool processAllDebugCommand(cJSON *jsoncfg, issueNodeData *issuestructNode, char *rfcbuf);
 bool processAllDeepSleepAwkMetricsCommands(cJSON *jsoncfg, issueNodeData *issuestructNode, char *rfcbuf);
 
+
+void persist_suffix_to_file(const char *suffix);
+char *read_suffix_from_file();
+void read_suffix_from_file_to_buf(char *buf, size_t buflen);
+void split_issue_type(const char *input, char *base, size_t base_len, char *suffix, size_t suffix_len);
+
 #ifdef __cplusplus
 }
 #endif
