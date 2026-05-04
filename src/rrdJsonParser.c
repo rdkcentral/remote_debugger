1a656c9312812fdcc35d4531099cecf96f29aeab
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2026-05-04 08:01:33 +0530

    Update rrdJsonParser.c

--- src/rrdJsonParser.c
+++ src/rrdJsonParser.c
@@ -107,7 +107,7 @@ void split_issue_type(const char *input, char *base, size_t base_len, char *suff
 	{
         return;
     }
-    RDK_LOG(RDK_LOG_INFO, LOG_REMDEBUG, "[%s:%d]: [INFO] split_issue_type called with input='%s'\n", __FUNCTION__, __LINE__, input);
+    RDK_LOG(RDK_LOG_DEBUG, LOG_REMDEBUG, "[%s:%d]: split_issue_type called with input='%s'\n", __FUNCTION__, __LINE__, input);
     const char *underscore = strchr(input, '_');
     if (underscore) 
 	{
@@ -124,7 +124,7 @@ void split_issue_type(const char *input, char *base, size_t base_len, char *suff
         base[base_len - 1] = '\0';
         suffix[0] = '\0';
     }
-    RDK_LOG(RDK_LOG_INFO, LOG_REMDEBUG, "[%s:%d]: [INFO] split_issue_type result: base='%s', suffix='%s'\n", __FUNCTION__, __LINE__, base, suffix);
+    RDK_LOG(RDK_LOG_DEBUG, LOG_REMDEBUG, "[%s:%d]: split_issue_type result: base='%s', suffix='%s'\n", __FUNCTION__, __LINE__, base, suffix);
 }
 
 
