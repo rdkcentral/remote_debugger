7b4d6e7320683484ef7c4bdaea700dc7b510bf00
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2026-09-01 22:01:06 +0530

    Update rrdMain.c

--- src/rrdMain.c
+++ src/rrdMain.c
@@ -67,10 +67,9 @@ void *RRDEventThreadFunc(void *arg)
             break;
         default:
             RDK_LOG(RDK_LOG_ERROR, LOG_REMDEBUG, "[%s:%d]: Invalid Message Type %d!!!\n", __FUNCTION__, __LINE__, rbuf->mtype);
-            free(rbuf->mdata);
-            free(rbuf);
             break;
         }
+        RRD_data_buff_deAlloc(rbuf);
 #if GTEST_ENABLE
         break;
 #endif
