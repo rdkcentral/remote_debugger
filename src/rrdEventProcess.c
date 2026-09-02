08fb4c7d9fdfdbaef141387d1bf62c2679183819
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2026-09-02 12:46:49 +0530

    Update rrdEventProcess.c

--- src/rrdEventProcess.c
+++ src/rrdEventProcess.c
@@ -350,7 +350,10 @@ static void processIssueType(data_buf *rbuf)
 		RDK_LOG(RDK_LOG_DEBUG, LOG_REMDEBUG, "[%s:%d]: Checking Issue from Static... \n", __FUNCTION__, __LINE__);
                 processIssueTypeInStaticProfile(rbuf, pIssueNode);
             }
-	    //CID-336989: Resource leak
+                free(pIssueNode->Node);
+                free(pIssueNode->subNode);
+                free(pIssueNode);
+                pIssueNode = NULL;
         }
         else
         {
