8ee1bed4215f19229e2a56a5baceb74b5770fadf
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2026-09-03 12:15:37 +0530

    Update uploadRRDLogs.c

--- src/uploadRRDLogs.c
+++ src/uploadRRDLogs.c
@@ -47,7 +47,7 @@ int rrd_upload_orchestrate(const char *upload_dir, const char *issue_type)
     
 #ifdef ENABLE_OTEL
     rdk_otlp_start_child_span("RRD_ctx", "rrd_upload_orchestrate");
-    RDK_LOG(RDK_LOG_DEBUG, "LOG.RDK.OTEL", "%s: [OTEL] Started child span for rrd_upload_orchestrate\n", __FUNCTION__);
+    RRD_OTEL_LOG(RDK_LOG_DEBUG, LOG_OTEL, "%s: [OTEL] Started child span for rrd_upload_orchestrate\n", __FUNCTION__);
 #endif
     RDK_LOG(RDK_LOG_INFO, LOG_REMDEBUG, "%s: Executing binary to upload Debug info of ISSUETYPE=%s\n", __FUNCTION__, issue_type);
 
@@ -144,7 +144,7 @@ int rrd_upload_orchestrate(const char *upload_dir, const char *issue_type)
 
     RDK_LOG(RDK_LOG_INFO, LOG_REMDEBUG, "%s: Exit\n", __FUNCTION__);
 #ifdef ENABLE_OTEL
-    RDK_LOG(RDK_LOG_DEBUG, "LOG.RDK.OTEL", "%s: [OTEL] Stopping child span for rrd_upload_orchestrate\n", __FUNCTION__);
+    RRD_OTEL_LOG(RDK_LOG_DEBUG, LOG_OTEL, "%s: [OTEL] Stopping child span for rrd_upload_orchestrate\n", __FUNCTION__);
     rdk_otlp_finish_child_span();
 #endif
     return 0;
