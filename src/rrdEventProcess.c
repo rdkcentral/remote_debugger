f69161f3ce30e60b5280481ae7fa120359efd61c
rdkdevpilot <svc-rdk-devpilot@cable.comcast.com>
2026-09-08 22:56:28 +0530

    Fix RESOURCE_LEAK in rrdEventProcess.c
    Defect ID: a33e9a79-0fa0-4f5a-b55c-8d9e9c3326f7

--- src/rrdEventProcess.c
+++ src/rrdEventProcess.c
@@ -342,6 +342,11 @@ static void processIssueType(data_buf *rbuf)
                 processIssueTypeInStaticProfile(rbuf, pIssueNode);
             }
 	    //CID-336989: Resource leak
+                if (pIssueNode->Node != NULL)
+                    free(pIssueNode->Node);
+                if (pIssueNode->subNode != NULL)
+                    free(pIssueNode->subNode);
+                free(pIssueNode);
         }
         else
         {
